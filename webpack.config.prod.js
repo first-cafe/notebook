65e1c86c27b5d3b63a1b2884981f643287c89af7
billvsme <994171686@qq.com>
2016-10-26 22:46:29 +0800

    Use Vue.js Markdown Editor Example

---
+++ webpack.config.prod.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack')
+
+
+module.exports = {
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js'],
+    alias: {vue: 'vue/dist/vue.js'}
+  },
+  entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader' }
+    ]
+  }
+}
